112b614bd6acf7e7893fdd95443231ae5edab330
Khuzaima Ahmed <khuzi1act@gmail.com>
2023-11-17 07:23:13 +0500

    Khuzaima/Added Post Controller

---
+++ CarRental/Controllers/PostController.cs
@@ -0,0 +1,44 @@
+﻿using CarRental.Context;
+using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PostController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts.ToListAsync();
+            return View(posts);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([Bind("Title,Content")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+    }
+
+
+}
